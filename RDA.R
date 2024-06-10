a458251352949a96405497bcfb61ecd19f9be78a
Emil Kriukov <77118598+mcrewcow@users.noreply.github.com>
2024-06-10 12:06:31 -0400

    Update RDA.R

--- RDA.R
+++ RDA.R
@@ -26,3 +26,17 @@ points(fig, 'sites', pch = 20, col = 'black', bg = 'black', cex = 0.8)
 text(fig, 'sites', col = 'red', cex = 0.8)
 fig
 
+
+plot(dca)
+summary(dca)
+summary(tbRDA)
+eigenvalues <- tbRDA$CA$eig
+total_variance <- sum(eigenvalues)
+variance_explained_RDA1 <- eigenvalues[1] / total_variance
+variance_explained_RDA2 <- eigenvalues[2] / total_variance
+print(paste("Variance explained by RDA1:", variance_explained_RDA1 * 100, "%"))
+print(paste("Variance explained by RDA2:", variance_explained_RDA2 * 100, "%"))
+anova_result <- anova.cca(tbRDA, permutations = 999)
+print(anova_result)
+anova_result <- anova.cca(tbRDA, permutations = 999, by = 'term')
+print(anova_result)
